ab5eddd19250ecef36163e355749f9afc2827831
Shoma SATO <noir.neo.04@gmail.com>
2017-04-03 23:25:37 +0900

    IEquatable

---
+++ Assets/Editor/Tests/SkillTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+public class SkillTest
+{
+
+    [Test]
+    public void SkillCountDictionaryEquate()
+    {
+        CollectionAssert.AreEquivalent(
+            Skill.SkillCountDictionaryEquate(Skills()),
+            Expect()
+        );
+    }
+
+    private List<Skill> Skills()
+    {
+        var skills = new List<Skill>();
+        skills.Add(new Skill(1, 1));
+        skills.Add(new Skill(1, 1));
+        skills.Add(new Skill(2, 1));
+        skills.Add(new Skill(2, 2));
+        skills.Add(new Skill(2, 2));
+        skills.Add(new Skill(2, 2));
+        return skills;
+    }
+
+    private Dictionary<Skill, int> Expect()
+    {
+        return new Dictionary<Skill, int>
+        {
+            {new Skill(1, 1), 2},
+            {new Skill(2, 1), 1},
+            {new Skill(2, 2), 3}
+        };
+    }
+
+}
---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Skill : IEquatable<Skill>
+{
+    public int Code { get; private set; }
+    public int Level { get; private set; }
+
+    public Skill(int code, int level)
+    {
+        Code = code;
+        Level = level;
+    }
+
+    public override int GetHashCode()
+    {
+        //return Tuple.Create(Code, Level).GetHashCode();
+        return Code.GetHashCode() ^ Level.GetHashCode();
+    }
+
+    bool IEquatable<Skill>.Equals(Skill other)
+    {
+        if (other == null)
+        {
+            return false;
+        }
+
+        return Code == other.Code && Level == other.Level;
+    }
+
+    public static IDictionary<Skill, int> SkillCountDictionaryEquate(List<Skill> skills)
+    {
+        var testCountDictionary = new Dictionary<Skill, int>();
+        foreach (var skill in skills)
+        {
+            if (!testCountDictionary.ContainsKey(skill))
+            {
+                testCountDictionary[skill] = 0;
+            }
+            testCountDictionary[skill]++;
+        }
+        return testCountDictionary;
+    }
+}

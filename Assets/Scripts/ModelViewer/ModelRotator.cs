b3210481a115665c365d27110d78599c74570f40
Shoma SATO <noir.neo.04@gmail.com>
2017-04-04 21:48:11 +0900

    Specify namespace

--- Assets/Scripts/ModelViewer/ModelRotator.cs
+++ Assets/Scripts/ModelViewer/ModelRotator.cs
@@ -2,29 +2,32 @@
 using UniRx;
 using Zenject;
 
-public class ModelRotator : MonoBehaviour
+namespace NeoC.ModelViewer
 {
-    [SerializeField] Transform xRotateRoot;
-    [SerializeField] Transform yRotateRoot;
-    [SerializeField] private float rotationSpeed;
-    [SerializeField] private float maxAngle;
-    [SerializeField] private float minAngle;
-
-    [Inject] private UIDragHandler dragHandler;
-
-    void Start()
+    public class ModelRotator : MonoBehaviour
     {
-        dragHandler.OnDragAsObservable()
-            .TakeUntil(dragHandler.OnEndDragAsObservable())
-            .Select(x => x.delta)
-            .RepeatUntilDestroy(this)
-            .Subscribe(x => Rotate(x));
-    }
+        [SerializeField] Transform xRotateRoot;
+        [SerializeField] Transform yRotateRoot;
+        [SerializeField] private float rotationSpeed;
+        [SerializeField] private float maxAngle;
+        [SerializeField] private float minAngle;
 
-    private void Rotate(Vector2 angles)
-    {
-        angles *= rotationSpeed;
-        yRotateRoot.Rotate(Vector3.up, -angles.x);
-        xRotateRoot.Rotate(Vector3.right, angles.y, minAngle, maxAngle);
+        [Inject] private UIDragHandler dragHandler;
+
+        void Start()
+        {
+            dragHandler.OnDragAsObservable()
+                .TakeUntil(dragHandler.OnEndDragAsObservable())
+                .Select(x => x.delta)
+                .RepeatUntilDestroy(this)
+                .Subscribe(x => Rotate(x));
+        }
+
+        private void Rotate(Vector2 angles)
+        {
+            angles *= rotationSpeed;
+            yRotateRoot.Rotate(Vector3.up, -angles.x);
+            xRotateRoot.Rotate(Vector3.right, angles.y, minAngle, maxAngle);
+        }
     }
 }

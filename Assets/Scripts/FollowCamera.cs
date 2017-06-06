ee03fa09ff23c8608b1305eca4819c831959b1af
Shoma SATO <noir.neo.04@gmail.com>
2017-06-06 23:31:30 +0900

    FollowCamera の挙動を修正

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,29 +1,19 @@
-﻿using NeoC;
-using UniRx;
-using UniRx.Triggers;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float smooth;
 
+    private Vector3 offset;
+
     void Start()
     {
-        StartFollow(transform, target, Time.deltaTime * smooth);
+        offset = transform.position - target.position;
     }
 
-    private void StartFollow(Transform transform, Transform target, float t)
+    void LateUpdate()
     {
-        if (transform == null || target == null)
-        {
-            return;
-        }
-
-        var offset = transform.position - target.position;
-        target.OnPositionChangeAsObservable()
-            .SelectMany(this.LateUpdateAsObservable())
-            .Subscribe(_ =>
-                transform.PositionLerp(target.position + offset, t));
+        transform.PositionLerp(target.position + offset, smooth * Time.deltaTime);
     }
 }

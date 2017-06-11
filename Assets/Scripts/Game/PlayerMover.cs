6025e583f08da5b6534221563d4c2be3be1dbe6e
Shoma SATO <noir.neo.04@gmail.com>
2017-06-11 22:57:08 +0900

    Move player character by rigidbody

--- Assets/Scripts/Game/PlayerMover.cs
+++ Assets/Scripts/Game/PlayerMover.cs
@@ -5,6 +5,7 @@ namespace NeoC.Game
     public class PlayerMover : MonoBehaviour
     {
         [SerializeField] private Animator _animator;
+        [SerializeField] private Rigidbody _rigidbody;
         [SerializeField] private float speed;
 
         public void Move(Vector2 move)
@@ -15,8 +16,8 @@ namespace NeoC.Game
 
         public void Move(Vector3 move)
         {
-            transform.position += move;
-            transform.LookToward(-move);
+            _rigidbody.position += move;
+            _rigidbody.LookToward(-move);
             _animator.SetFloat("velocity", move.sqrMagnitude);
         }
     }

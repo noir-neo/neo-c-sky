3bbcdcfb78b0b6bad7cd3a643e981c89b23754f6
Shoma SATO <noir.neo.04@gmail.com>
2017-06-27 00:40:45 +0900

    カメラをドリーインアウトできるように

---
+++ Assets/Scripts/ModelViewer/TransformDolly.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ModelViewer
+{
+    public sealed class TransformDolly : PinchObserverBase
+    {
+        [SerializeField] Transform dollyRoot;
+        [SerializeField] private Vector3 axis;
+        [SerializeField] private Vector3 minPosition;
+        [SerializeField] private Vector3 maxPosition;
+
+        protected override void OnPinch(float magnitude)
+        {
+            var pos = dollyRoot.localPosition;
+            pos += axis * magnitude;
+            dollyRoot.localPosition = pos.Clamp(minPosition, maxPosition);
+        }
+    }
+}

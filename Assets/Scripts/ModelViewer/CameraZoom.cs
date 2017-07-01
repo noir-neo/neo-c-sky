1947fa0755b5325ffad92e50141e2cbac5c47f71
Shoma SATO <noir.neo.04@gmail.com>
2017-07-01 16:52:03 +0900

    カメラを FoV でズームインアウトできるように

---
+++ Assets/Scripts/ModelViewer/CameraZoom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ModelViewer
+{
+    public sealed class CameraZoom : PinchObserverBase
+    {
+        [SerializeField] Camera _camera;
+        [SerializeField] private float speed;
+        [SerializeField] private float minFieldOfView;
+        [SerializeField] private float maxFieldOfView;
+
+        protected override void OnPinch(float magnitude)
+        {
+            float fieldOfView = _camera.fieldOfView;
+            fieldOfView += magnitude * -speed;
+            _camera.fieldOfView = Mathf.Clamp(fieldOfView, minFieldOfView, maxFieldOfView);
+        }
+    }
+}

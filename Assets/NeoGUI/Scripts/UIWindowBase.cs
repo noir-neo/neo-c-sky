ab141f6d7f3dddcfd92b76ae3d14dab94945c57f
Shoma SATO <noir.neo.04@gmail.com>
2017-08-31 00:09:30 +0900

    window の土台

---
+++ Assets/NeoGUI/Scripts/UIWindowBase.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace NeoGUI
+{
+    public abstract class UIWindowBase<T> : UIWindowBase
+    {
+        public virtual void Open(T t)
+        {
+            base.Open();
+        }
+    }
+
+    public abstract class UIWindowBase : MonoBehaviour
+    {
+        public virtual void Open()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public virtual void Suspend()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public virtual void Close()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+

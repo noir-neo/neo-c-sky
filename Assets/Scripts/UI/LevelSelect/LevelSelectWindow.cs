b7caee4e1c369571a0c651c353820fb8fa36c50e
Shoma SATO <noir.neo.04@gmail.com>
2017-09-08 23:44:14 +0900

    Load and unload Level

--- Assets/Scripts/UI/LevelSelect/LevelSelectWindow.cs
+++ Assets/Scripts/UI/LevelSelect/LevelSelectWindow.cs
@@ -1,9 +1,34 @@
-﻿using NeoGUI;
+﻿using System.Linq;
+using NeoGUI;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace NeoC.UI
 {
     public class LevelSelectWindow : UIWindowBase
     {
+        [SerializeField] private Button[] buttons;
 
+        void Start()
+        {
+            // FIXME: このクラス UI なのか presenter よくわからん
+            OnLevelSelected()
+                .StartWith(0)
+                .DistinctUntilChanged()
+                .SelectMany(x => LevelLoader.LoadLevelAsObservable(x)
+                    .Select(_ => x))
+                .Pairwise()
+                .SelectMany(x => LevelLoader.UnloadLevelAsObservable(x.Previous))
+                .Subscribe()
+                .AddTo(this);
+        }
+
+        private IObservable<int> OnLevelSelected()
+        {
+            return buttons.Select((b, i) => b.OnClickAsObservable()
+                    .Select(_ => i))
+                .Merge();
+        }
     }
 }

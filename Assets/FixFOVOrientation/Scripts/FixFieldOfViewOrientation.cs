9259f0b492dd6dd639aa03a3921b3d73e00f0d4d
Shoma SATO <noir.neo.04@gmail.com>
2017-09-24 03:00:55 +0900

    画面横にしたとき fov を調整して縦と同じ大きさに見えるようにする component

---
+++ Assets/FixFOVOrientation/Scripts/FixFieldOfViewOrientation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace FixFOVOrientation
+{
+    [RequireComponent(typeof(Camera)), ExecuteInEditMode]
+    public class FixFieldOfViewOrientation : MonoBehaviour
+    {
+        [SerializeField, HideInInspector] private Camera _camera;
+        [Range(1, 179)] public float fieldOfView;
+
+        private bool IsLandscape => _camera.aspect > 1;
+
+        void Update()
+        {
+            _camera.fieldOfView = FixedFieldOfView(fieldOfView, _camera.aspect, IsLandscape);
+        }
+
+        private static float FixedFieldOfView(float fieldOfView, float aspect, bool isLandscape)
+        {
+            if (isLandscape) return fieldOfView / aspect;
+            return fieldOfView;
+        }
+
+        void OnValidate()
+        {
+            _camera = GetComponent<Camera>();
+        }
+    }
+}

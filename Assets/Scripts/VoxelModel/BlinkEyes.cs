681a132ce33b1d004b10084e17052617560da20d
Shoma SATO <noir.neo.04@gmail.com>
2017-12-16 15:58:10 +0900

    Scripts/VoxelModel 作成

---
+++ Assets/Scripts/VoxelModel/BlinkEyes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace VoxelModel
+{
+    [RequireComponent(typeof(Renderer))]
+    public class BlinkEyes : MonoBehaviour
+    {
+        private Material material;
+
+        void Start()
+        {
+            material = GetComponent<Renderer>().material;
+            StartCoroutine(Blink());
+        }
+
+        private IEnumerator Blink()
+        {
+            yield return new WaitForSeconds(3.0f);
+            material.SetFloat("_Ratio", 1.0f);
+            yield return new WaitForSeconds(0.1f);
+            material.SetFloat("_Ratio", 0.0f);
+            yield return Blink();
+        }
+    }
+}

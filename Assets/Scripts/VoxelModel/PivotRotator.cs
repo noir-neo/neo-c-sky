4f5b208bea39d778d27e15a55bd1cc48b6c45a9a
Shoma SATO <noir.neo.04@gmail.com>
2017-12-16 19:59:55 +0900

    Pivot を指定して回転できる component

---
+++ Assets/Scripts/VoxelModel/PivotRotator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace VoxelModel
+{
+    [ExecuteInEditMode]
+    public class PivotRotator : MonoBehaviour
+    {
+        private static readonly float gizmoSize = 0.1f;
+        private static readonly Color gizmoColor = Color.yellow;
+
+        [SerializeField] private Vector3 point;
+        public Vector3 rotation;
+
+        private Vector3 WorldPivot()
+        {
+            return transform.TransformPoint(point);
+        }
+
+        private Quaternion WorldRotation()
+        {
+            return  transform.rotation * Quaternion.Inverse(transform.localRotation) * Quaternion.Euler(rotation);
+        }
+
+        void Update()
+        {
+            transform.SetRotationAround(WorldPivot(), WorldRotation());
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = gizmoColor;
+            Gizmos.DrawWireSphere(WorldPivot(), gizmoSize);
+        }
+    }
+
+    public static class TransformExtensions
+    {
+        public static void SetRotationAround(this Transform transform, Vector3 point, Quaternion rotation)
+        {
+            var position = rotation * Quaternion.Inverse(transform.rotation) * (transform.position - point) + point;
+            transform.SetPositionAndRotation(position, rotation);
+        }
+    }
+}

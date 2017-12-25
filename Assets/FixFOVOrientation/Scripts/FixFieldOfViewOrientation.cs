18b0d805c994e40ed2c7197146985c052b565d7f
Shoma SATO <noir.neo.04@gmail.com>
2017-12-26 01:01:01 +0900

    カメラの回転によって被写体の大きさが変わらないようにする ortho 対応

--- Assets/FixFOVOrientation/Scripts/FixFieldOfViewOrientation.cs
+++ Assets/FixFOVOrientation/Scripts/FixFieldOfViewOrientation.cs
@@ -7,23 +7,47 @@ namespace FixFOVOrientation
     {
         [SerializeField, HideInInspector] private Camera _camera;
         [Range(1, 179)] public float fieldOfView;
-
-        private bool IsLandscape => _camera.aspect > 1;
+        public float orthographicSize;
 
         void Update()
         {
-            _camera.fieldOfView = FixedFieldOfView(fieldOfView, _camera.aspect, IsLandscape);
-        }
-
-        private static float FixedFieldOfView(float fieldOfView, float aspect, bool isLandscape)
-        {
-            if (isLandscape) return fieldOfView / aspect;
-            return fieldOfView;
+            if (_camera.orthographic)
+            {
+                _camera.SetFixedOrthographicSize(orthographicSize);
+            }
+            else
+            {
+                _camera.SetFixedFieldOfView(fieldOfView);
+            }
         }
 
         void OnValidate()
         {
             _camera = GetComponent<Camera>();
+        }
+    }
+
+    public static class CameraExtensions
+    {
+        public static void SetFixedOrthographicSize(this Camera camera, float orthographicSize)
+        {
+            camera.orthographicSize = camera.FixedViewSize(orthographicSize);
+        }
+
+        public static void SetFixedFieldOfView(this Camera camera, float fieldOfView)
+        {
+            camera.fieldOfView = camera.FixedViewSize(fieldOfView);
+        }
+
+        private static float FixedViewSize(this Camera camera, float value)
+        {
+            if (camera.IsLandscape()) return value / camera.aspect;
+            return value;
+        }
+
+        private static bool IsLandscape(this Camera camera)
+        {
+            return camera.aspect > 1;
         }
     }
 }
